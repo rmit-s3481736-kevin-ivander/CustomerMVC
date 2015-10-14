34b8f023735122fc20b47f4bd41b116cd90ccfb0
Kevin Ivander <s3481736@student.rmit.edu.au>
2015-10-14 19:38:10 +1100

    MVC

---
+++ CustomerMVC/Controllers/MovieController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CustomerMVC.Models;
+
+namespace CustomerMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        private Assignment2Entities db = new Assignment2Entities();
+
+        // GET: /Movie/
+        public ActionResult Index()
+        {
+            return View(db.Movies1.ToList());
+        }
+
+        // GET: /Movie/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movies movies = db.Movies1.Find(id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        // GET: /Movie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Movie/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Movie_ID,Movie_Title,Short_Description,Long_Description,Poster,Duration,Rating,Price")] Movies movies)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies1.Add(movies);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movies);
+        }
+
+        // GET: /Movie/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movies movies = db.Movies1.Find(id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        // POST: /Movie/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Movie_ID,Movie_Title,Short_Description,Long_Description,Poster,Duration,Rating,Price")] Movies movies)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movies).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movies);
+        }
+
+        // GET: /Movie/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movies movies = db.Movies1.Find(id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        // POST: /Movie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movies movies = db.Movies1.Find(id);
+            db.Movies1.Remove(movies);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
