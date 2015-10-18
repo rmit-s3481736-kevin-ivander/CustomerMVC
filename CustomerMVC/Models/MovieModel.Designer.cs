dd2637bc1f13f7db2090846a56e4e4222d6955f2
Maximilian De Oliveira-Spiker <s3476696@student.rmit.edu.au>
2015-10-18 17:13:38 +1100

    EF

--- CustomerMVC/Models/MovieModel.Designer.cs
+++ CustomerMVC/Models/MovieModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model '\\vmware-host\Shared Folders\Desktop\CustomerMVC\CustomerMVC\Models\MovieModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Max\Documents\CustomerMVC\CustomerMVC\Models\MovieModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
