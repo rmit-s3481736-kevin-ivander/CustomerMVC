dbd41a5f4f83493af2a76631b77f753fc14033e0
Kevin Ivander <s3481736@student.rmit.edu.au>
2015-10-19 23:30:57 +1100

    script

---
+++ cineplexmovie.sql
@@ -0,0 +1,64 @@
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1004, 1);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1005, 1);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1006, 1);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1007, 1);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1008, 1);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1004, 2);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1005, 2);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1006, 2);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1007, 2);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1008, 2);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1004, 3);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1005, 3);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1006, 3);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1007, 3);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1008, 3);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1004, 4);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1005, 4);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1006, 4);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1007, 4);
+
+insert into CineplexMovie (CineplexID, Movie_ID) 
+values (1008, 4);
+
+
+
+
