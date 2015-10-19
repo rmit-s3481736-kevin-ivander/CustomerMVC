b23e1282b5a151c1526bf5a97127005e05d4bb25
Kevin Ivander <s3481736@student.rmit.edu.au>
2015-10-19 20:39:04 +1100

    validation v1

--- CustomerMVC/Models/Payment.cs
+++ CustomerMVC/Models/Payment.cs
@@ -11,14 +11,22 @@ namespace CustomerMVC.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Payment
     {
+         [Required]
         public string First_Name { get; set; }
+         [Required]
         public string Last_Name { get; set; }
+         [Required]
         public string CardNo { get; set; }
+         [Required]
+         [StringLength(3)]
         public string CCV { get; set; }
+         [Required]
         public string CardType { get; set; }
+         [Required]
         public Nullable<System.DateTime> ExpiredDate { get; set; }
     }
 }
